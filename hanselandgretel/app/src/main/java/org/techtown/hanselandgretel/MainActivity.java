e6216e97647fa5019c4d53cbdfc5d89e399f37e0
Woong-Jin Yun <sw2410@naver.com>
2019-08-23 12:28:57 +0900

    hansel0823_1228
    여기에 카메라  합쳐줘요

--- hanselandgretel/app/src/main/java/org/techtown/hanselandgretel/MainActivity.java
+++ hanselandgretel/app/src/main/java/org/techtown/hanselandgretel/MainActivity.java
@@ -3,8 +3,12 @@ package org.techtown.hanselandgretel;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
@@ -85,6 +89,9 @@ public class MainActivity extends AppCompatActivity {
         //gsp 설정 해줘야함
         gpscur.setText(" 12345"+" . "+"233");
         gpsgoal.setText(" 19845"+" . "+"4423");
+
+        //현재 위치정보 업데이트 메서드**************************
+        startLocationService();
 
        //답설정
 
@@ -227,5 +234,78 @@ public class MainActivity extends AppCompatActivity {
 
         alert.show();
     }
+
+    private void startLocationService() {
+        // 위치 관리자 객체 참조
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // 위치 정보를 받을 리스너 생성
+        GPSListener gpsListener = new GPSListener();
+        long minTime = 1000;
+        float minDistance = 0;
+
+        try {
+            // GPS를 이용한 위치 요청
+            manager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    minTime,
+                    minDistance,
+                    gpsListener);
+
+            // 네트워크를 이용한 위치 요청
+            /*manager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    minTime,
+                    minDistance,
+                    gpsListener)*/;
+
+            // 위치 확인이 안되는 경우에도 최근에 확인된 위치 정보 먼저 확인
+            Location lastLocation = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            Log.d("@@@","Lastlocation : "+ lastLocation);
+            if (lastLocation != null) {
+                Double latitude = lastLocation.getLatitude();
+                Double longitude = lastLocation.getLongitude();
+
+                gpscur.setText("" + latitude + " , " + longitude);
+                Toast.makeText(getApplicationContext(), "Last Known Location : " + "Latitude : " + latitude + "\nLongitude:" + longitude, Toast.LENGTH_LONG).show();
+            }
+        } catch(SecurityException ex) {
+            ex.printStackTrace();
+            Log.d("@@@","error : "+ ex.toString());
+        }
+
+        Toast.makeText(getApplicationContext(), "위치 확인이 시작되었습니다. 로그를 확인하세요.", Toast.LENGTH_SHORT).show();
+
+    }
+
+    /**
+     * 리스너 클래스 정의
+     */
+    private class GPSListener implements LocationListener {
+        /**
+         * 위치 정보가 확인될 때 자동 호출되는 메소드
+         */
+        public void onLocationChanged(Location location) {
+            Double latitude = location.getLatitude();
+            Double longitude = location.getLongitude();
+
+            String msg = "Latitude : "+ latitude + "\nLongitude:"+ longitude;
+            Log.i("GPSListener", msg);
+
+            gpscur.setText("내 위치 : " + latitude + ", " + longitude);
+            Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+        }
+
+        public void onProviderDisabled(String provider) {
+        }
+
+        public void onProviderEnabled(String provider) {
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+    }
+
 
 }
